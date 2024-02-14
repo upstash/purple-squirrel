dec5858e65990bc50d18af4a5dbcc5bc12d565c7
yunusemreozdemir <emreozdemir@mail.com>
2024-02-14 21:05:41 +0300

    feat: create saved queries subpage files

---
+++ components/recruiters/console/SavedQueriesSubpage/SavedQueriesSubpage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './SavedQueriesSubpage.module.css';
+
+const SavedQueriesSubpage = () => {
+  return (
+    <div>
+      <h1>Saved Queries</h1>
+    </div>
+  );
+};
+
+export default SavedQueriesSubpage;
