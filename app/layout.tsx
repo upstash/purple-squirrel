dbcbafba019ce93dcaf20a0d8af7a78b1b7e23cd
yunusemreozdemir <emreozdemir@mail.com>
2024-02-21 23:05:47 +0300

    feat: install next ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
+import {Providers} from "./providers";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (        
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className='dark'>
+      <body>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
     </html>
   )
 }
