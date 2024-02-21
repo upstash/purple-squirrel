3e83b2216c0f9c7f642a1564e61116cc74ae240d
yunusemreozdemir <emreozdemir@mail.com>
2024-02-22 02:16:07 +0300

    fix: add necessary routes to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,14 +5,13 @@ import {nextui} from "@nextui-org/react";
 const config = {
   content: [
     // ...
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {
-      colors: {
-        'primary': '#9353d3',
-      }
-    }
+    extend: {}
   },
   darkMode: "class",
   plugins: [nextui()]
