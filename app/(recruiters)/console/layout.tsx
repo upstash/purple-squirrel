8ee502e5f577b65e21c9baa15d3d395ee7edf1f6
yunusemreozdemir <emreozdemir@mail.com>
2024-02-22 00:18:06 +0300

    feat: add recruiter console navbar

--- app/(recruiters)/console/layout.tsx
+++ app/(recruiters)/console/layout.tsx
@@ -1,11 +1,23 @@
+'use client';
+
+import React from "react";
+import {Tabs, Tab} from "@nextui-org/react";
+import {usePathname} from "next/navigation";
+
 export default function ConsoleLayout({
     children,
   }: {
     children: React.ReactNode
   }) {
+    const pathname = usePathname();
+
     return (
       <section>   
-        <h1>Console Navbar</h1>
+        <Tabs key={"primary"} color={"primary"} aria-label="Tabs colors" selectedKey={pathname} radius="full">
+          <Tab key="/console/query-terminal" title="Query Terminal" href="/console/query-terminal"/>
+          <Tab key="/console/recent-queries" title="Recent Queries" href="/console/recent-queries"/>
+          <Tab key="/console/saved-queries" title="Saved Queries" href="/console/saved-queries"/>
+        </Tabs>
         {children}
       </section>
     )
