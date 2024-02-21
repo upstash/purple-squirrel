c90c5a6b5fd5bc22fbe3c24db37315733c2dfccb
yunusemreozdemir <emreozdemir@mail.com>
2024-02-22 02:16:48 +0300

    feat: styling of the console navbar

--- app/(recruiters)/console/layout.tsx
+++ app/(recruiters)/console/layout.tsx
@@ -12,12 +12,14 @@ export default function ConsoleLayout({
     const pathname = usePathname();
 
     return (
-      <section>   
-        <Tabs key={"primary"} color={"primary"} aria-label="Tabs colors" selectedKey={pathname} radius="full">
-          <Tab key="/console/query-terminal" title="Query Terminal" href="/console/query-terminal"/>
-          <Tab key="/console/recent-queries" title="Recent Queries" href="/console/recent-queries"/>
-          <Tab key="/console/saved-queries" title="Saved Queries" href="/console/saved-queries"/>
-        </Tabs>
+      <section>
+        <header className="flex items-center justify-center bg-zinc-900 m-unit-2 p-unit-2 rounded-xl">
+          <Tabs key={"primary"} color={"secondary"} aria-label="Tabs colors" selectedKey={pathname} radius="full">
+            <Tab key="/console/query-terminal" title="Query Terminal" href="/console/query-terminal"/>
+            <Tab key="/console/recent-queries" title="Recent Queries" href="/console/recent-queries"/>
+            <Tab key="/console/saved-queries" title="Saved Queries" href="/console/saved-queries"/>
+          </Tabs>
+        </header>
         {children}
       </section>
     )
