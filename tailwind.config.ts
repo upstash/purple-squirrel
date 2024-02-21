77fafc2b15a0525fccc4026d6d55bdd52038d3b7
yunusemreozdemir <emreozdemir@mail.com>
2024-02-22 00:30:12 +0300

    feat: change primary color to purple

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,15 @@ const config = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#9353d3',
+      }
+    }
   },
   darkMode: "class",
   plugins: [nextui()]
 }
 
 export default config;
+
