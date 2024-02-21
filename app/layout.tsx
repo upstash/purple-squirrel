39fd4f5de9ca7670742617a9a5cf070b8359be46
yunusemreozdemir <emreozdemir@mail.com>
2024-02-22 02:16:27 +0300

    feat: temporary background change to zinc

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (        
-    <html lang="en" className='dark'>
+    <html lang="en" className='dark bg-zinc-950'>
       <body>
         <Providers>
           {children}
