10093d99bb2ce987279d40239aa4030414fda9ad
yunusemreozdemir <emreozdemir@mail.com>
2024-02-22 22:54:05 +0300

    feat: make dark mode switch adapt to tab colors

--- app/components/DarkModeSwitch.jsx
+++ app/components/DarkModeSwitch.jsx
@@ -3,12 +3,12 @@ import {Switch} from "@nextui-org/switch";
 import {MoonIcon} from "./MoonIcon";
 import {SunIcon} from "./SunIcon";
 
-export function DarkModeSwitch({ defaultSelected, onChange }) {
+export function DarkModeSwitch({ defaultSelected, color, onChange }) {
   return (
     <Switch
       defaultSelected={defaultSelected}
       size="lg"
-      color="secondary"
+      color={color}
       onChange={onChange}
       thumbIcon={({ isSelected, className }) =>
         isSelected ? (
