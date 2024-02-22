48ce4f011d8ddb34b5dd8b38f0db26b46a09e97d
yunusemreozdemir <emreozdemir@mail.com>
2024-02-22 04:36:51 +0300

    feat: add theme switch button

--- app/(recruiters)/console/layout.tsx
+++ app/(recruiters)/console/layout.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import {Tabs, Tab} from "@nextui-org/react";
 import {usePathname} from "next/navigation";
+import { ThemeSwitcher } from "@/app/components/ThemeSwitcher";
 
 export default function ConsoleLayout({
     children,
@@ -13,12 +14,21 @@ export default function ConsoleLayout({
 
     return (
       <section>
-        <header className="flex items-center justify-center bg-zinc-900 m-unit-2 p-unit-2 rounded-xl">
-          <Tabs key={"primary"} color={"secondary"} aria-label="Tabs colors" selectedKey={pathname} radius="full">
-            <Tab key="/console/query-terminal" title="Query Terminal" href="/console/query-terminal"/>
-            <Tab key="/console/recent-queries" title="Recent Queries" href="/console/recent-queries"/>
-            <Tab key="/console/saved-queries" title="Saved Queries" href="/console/saved-queries"/>
-          </Tabs>
+        <header className="flex items-center bg-default-50 m-unit-3 py-unit-2 px-unit-3 rounded-xl">
+          <div className="flex-1 flex items-center justify-start">
+            <div className="bg-secondary mx-unit-1 p-unit-4 rounded-full"></div>
+            <h1 className="text-xl">Purple Squirrel</h1>
+          </div>
+          <div className="flex-1 flex items-center justify-center">
+            <Tabs key={"primary"} color={"secondary"} aria-label="Tabs colors" selectedKey={pathname} radius="full">
+              <Tab key="/console/query-terminal" title="Query Terminal" href="/console/query-terminal"/>
+              <Tab key="/console/recent-queries" title="Recent Queries" href="/console/recent-queries"/>
+              <Tab key="/console/saved-queries" title="Saved Queries" href="/console/saved-queries"/>
+            </Tabs>
+          </div>
+          <div className="flex-1 flex items-center justify-end">
+            <ThemeSwitcher/>
+          </div>
         </header>
         {children}
       </section>
