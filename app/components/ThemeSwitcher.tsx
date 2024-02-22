6c1a550851f4c76d745ffdbd70a56952c0c6af9b
yunusemreozdemir <emreozdemir@mail.com>
2024-02-22 22:54:25 +0300

    feat: make dark mode switch adapt to tab colors

--- app/components/ThemeSwitcher.tsx
+++ app/components/ThemeSwitcher.tsx
@@ -4,7 +4,7 @@ import {useTheme} from "next-themes";
 import { useEffect, useState } from "react";
 import {DarkModeSwitch} from "./DarkModeSwitch";
 
-export function ThemeSwitcher() {
+export function ThemeSwitcher( { color }: { color: string } ) {
   const [mounted, setMounted] = useState(false)
   const { theme, setTheme } = useTheme()
 
@@ -15,6 +15,6 @@ export function ThemeSwitcher() {
   if(!mounted) return null
 
   return (
-    <DarkModeSwitch defaultSelected={theme === 'light' ? 1 : 0} onChange={() => setTheme(theme === 'light' ? 'dark': 'light')} />
+    <DarkModeSwitch defaultSelected={theme === 'light' ? 1 : 0} color={color} onChange={() => setTheme(theme === 'light' ? 'dark': 'light')} />
   )
 };
