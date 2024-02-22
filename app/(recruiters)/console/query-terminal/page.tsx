57abcde7bd3445ce4bfcf45c51611f620e752ead
yunusemreozdemir <emreozdemir@mail.com>
2024-02-22 14:43:34 +0300

    feat: query bar and search, save, settings buttons

--- app/(recruiters)/console/query-terminal/page.tsx
+++ app/(recruiters)/console/query-terminal/page.tsx
@@ -1,3 +1,36 @@
+import {Input} from "@nextui-org/input";
+import {Button, ButtonGroup} from "@nextui-org/button";
+import SearchIcon from '@mui/icons-material/Search';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+
 export default function Page() {
-    return <h1>Query Terminal</h1>
+    return (
+      <div className="flex flex-col h-full">
+        <div className="flex-initial pb-unit-2">
+          <div className="bg-default-50 flex items-center rounded-xl px-unit-3 py-unit-3 h-full">
+            <div className="flex-auto px-unit-1">
+              <Input label="Query" size="sm"/>
+            </div>
+            <div className="flex-initial px-unit-1">
+              <Button color="success" size="md" startContent={<SearchIcon />}>Search</Button>
+            </div>
+            <div className="flex-initial px-unit-1">
+              <Button isIconOnly color="secondary" size="md"><BookmarkBorderIcon /></Button>
+            </div>
+            <div className="flex-initial px-unit-1">
+              <Button isIconOnly color="secondary" size="md"><SettingsOutlinedIcon /></Button>
+            </div>
+          </div>
+        </div>
+        <div className="flex-auto flex h-full">
+          <div className="flex-1 pr-unit-2 pt-unit-2">
+            <div className="bg-default-50 rounded-xl h-full"></div>
+          </div>
+          <div className="flex-1 pl-unit-2 pt-unit-2">
+            <div className="bg-default-50 rounded-xl h-full"></div>
+          </div>
+        </div>
+      </div>
+    )
   }
