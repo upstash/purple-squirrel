0fc24996ff0a84854599b2c560c681b7f6423573
yunusemreozdemir <emreozdemir@mail.com>
2024-02-22 22:51:47 +0300

    feat: temporarily change purple squirrel icon, implement different colors for different tabs

--- app/(recruiters)/console/layout.tsx
+++ app/(recruiters)/console/layout.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import {Tabs, Tab} from "@nextui-org/react";
 import {usePathname} from "next/navigation";
 import { ThemeSwitcher } from "@/app/components/ThemeSwitcher";
+import {SquirrelIcon} from '@primer/octicons-react'
+import path from "path";
 
 export default function ConsoleLayout({
     children,
@@ -11,24 +13,27 @@ export default function ConsoleLayout({
     children: React.ReactNode
   }) {
     const pathname = usePathname();
+    const varColor = ((pathname === "/console/query-terminal") ? "secondary" : (pathname === "/console/recent-queries") ? "warning" : "primary");
 
     return (
       <section className="flex flex-col box-border h-screen">
         <header className="flex-initial p-unit-4">
-          <div className="flex items-center bg-default-50 py-unit-2 px-unit-3 rounded-xl">
+          <div className="flex items-center bg-default-50 py-unit-2 px-unit-1 rounded-xl">
             <div className="flex-1 flex items-center justify-start">
-              <div className="bg-secondary mx-unit-1 p-unit-3 rounded-full"></div>
-              <h1 className="text-large">Purple Squirrel</h1>
+              <div className="text-secondary pl-unit-3 pr-unit-1">
+                <SquirrelIcon size={32} className="scale-x-[-1]" />
+              </div>
+              <h1 className="text-xl">Purple Squirrel</h1>
             </div>
             <div className="flex-1 flex items-center justify-center">
-              <Tabs key={"primary"} color={"secondary"} aria-label="Tabs colors" selectedKey={pathname} radius="full">
+              <Tabs key={"primary"} color={varColor} aria-label="Tabs colors" selectedKey={pathname} radius="full">
                 <Tab key="/console/query-terminal" title="Query Terminal" href="/console/query-terminal"/>
                 <Tab key="/console/recent-queries" title="Recent Queries" href="/console/recent-queries"/>
                 <Tab key="/console/saved-queries" title="Saved Queries" href="/console/saved-queries"/>
               </Tabs>
             </div>
             <div className="flex-1 flex items-center justify-end">
-              <ThemeSwitcher/>
+              <ThemeSwitcher color={varColor} />
             </div>
           </div>
         </header>
