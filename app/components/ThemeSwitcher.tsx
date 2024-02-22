511f679e2e06dab0c31164102fa574597df48297
yunusemreozdemir <emreozdemir@mail.com>
2024-02-22 04:38:10 +0300

    feat: change from buttons to dark mode switch

--- app/components/ThemeSwitcher.tsx
+++ app/components/ThemeSwitcher.tsx
@@ -2,6 +2,7 @@
 
 import {useTheme} from "next-themes";
 import { useEffect, useState } from "react";
+import {DarkModeSwitch} from "./DarkModeSwitch";
 
 export function ThemeSwitcher() {
   const [mounted, setMounted] = useState(false)
@@ -14,10 +15,6 @@ export function ThemeSwitcher() {
   if(!mounted) return null
 
   return (
-    <div>
-      The current theme is: {theme}
-      <button onClick={() => setTheme('light')}>Light Mode</button>
-      <button onClick={() => setTheme('dark')}>Dark Mode</button>
-    </div>
+    <DarkModeSwitch defaultSelected={theme === 'light' ? 1 : 0} onChange={() => setTheme(theme === 'light' ? 'dark': 'light')} />
   )
 };
