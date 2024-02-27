ab142a3303d9a1ddcf013d57b5ec0e137d58a8d9
yunusemreozdemir <emreozdemir@mail.com>
2024-02-27 18:22:58 +0300

    feat: add ApplicantsTable and ApplicantCard to the query terminal page

--- app/(recruiters)/console/query-terminal/page.tsx
+++ app/(recruiters)/console/query-terminal/page.tsx
@@ -4,14 +4,16 @@ import {Tooltip} from "@nextui-org/tooltip";
 import SearchIcon from '@mui/icons-material/Search';
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import ApplicantsTable from "./ApplicantsTable";
+import ApplicantCard from "./ApplicantCard";
 
 export default function Page() {
     return (
       <div className="flex flex-col h-full">
         <div className="flex-initial pb-unit-2">
-          <div className="bg-default-50 flex items-center rounded-xl p-unit-2 h-full">
+          <div className="bg-default-50 flex items-center rounded-xl px-unit-2 py-unit-3 h-full">
             <div className="flex-auto px-unit-1">
-              <Input label="Query" size="sm"/>
+              <Input label={null} size="sm" placeholder="Enter a query..." classNames={{inputWrapper: "h-10"}}/>
             </div>
             <div className="flex-initial px-unit-1">
               <Button color="success" size="md" startContent={<SearchIcon />}>Search</Button>
@@ -29,11 +31,15 @@ export default function Page() {
           </div>
         </div>
         <div className="flex-auto flex h-full">
-          <div className="flex-1 pr-unit-2 pt-unit-2">
-            <div className="bg-default-50 rounded-xl h-full"></div>
+          <div className="flex-[72_1_0%] pr-unit-2 pt-unit-2">
+            <div className="flex flex-col bg-default-50 rounded-xl h-full p-unit-3">
+              <ApplicantsTable />
+            </div>
           </div>
-          <div className="flex-1 pl-unit-2 pt-unit-2">
-            <div className="bg-default-50 rounded-xl h-full"></div>
+          <div className="flex-[28_1_0%] pl-unit-2 pt-unit-2">
+            <div className="bg-default-50 rounded-xl h-full p-unit-3">
+              <ApplicantCard />
+            </div>
           </div>
         </div>
       </div>
