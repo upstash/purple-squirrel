f092362095ad786f91cfa6dc7512487820d60746
yunusemreozdemir <emreozdemir@mail.com>
2024-03-24 04:17:03 +0300

    init: create Applicant type

---
+++ types/types.ts
@@ -0,0 +1,69 @@
+export type Applicant = {
+    id: number | null;
+
+    applicantInfo: {
+      name: string;
+      age: number;
+      yoe: number;
+      contact: {
+        email: string;
+        phone: string;
+      }
+      location: string;
+      countryCode: string;
+      latestEducation: {
+        ongoing: boolean;
+        degree: string;
+        subject: string;
+        university: string;
+        graduation: {
+          month: number;
+          year: number;
+        }
+      }
+      urls: {
+        website: string;
+        linkedin: string;
+        github: string;
+      }
+    }
+
+    recruitmentInfo: {
+      stars: number | null;
+      notes: string | null;
+      status:
+        | "newApply"
+        | "screening"
+        | "assessment"
+        | "interview"
+        | "consideration"
+        | "offer"
+        | "onboarding"
+        | "hired";
+    }
+
+    applicationInfo: {
+      date: Date;
+      method: "mail" | "upload";
+      team: string;
+      role: string;
+      mailInfo?: {
+        date: Date;
+        from: string;
+        subject: string;
+        body: string;
+      }
+    }
+
+    resumeInfo: {
+      uploadthing: {
+        key: string;
+        url: string;
+      }
+      partExists: {
+        education: boolean;
+        experience: boolean;
+        projects: boolean;
+      }
+    }
+  };
