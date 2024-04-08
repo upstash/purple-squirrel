d386ea3db77d178b5413efc53681ab83ed8b2878
yunusemreozdemir <emreozdemir@mail.com>
2024-04-08 11:18:45 +0300

    init: create push-recent-query route

---
+++ app/api/push-recent-query/route.ts
@@ -0,0 +1,16 @@
+import { Redis } from '@upstash/redis';
+import type { NextRequest } from 'next/server';
+
+const redis = new Redis({
+    url: process.env.UPSTASH_REDIS_REST_URL as string,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN as string,
+});
+
+export async function POST(req: NextRequest) {
+    const data = await req.json();
+
+    await redis.lpush("recent:queries", data); 
+    await redis.ltrim("recent:queries", 0, 49); 
+
+    return Response.json({ status: 200, message: "Success" });
+}
