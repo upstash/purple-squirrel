55c88d3540d4bd409e9fa3b7fb8c7ac9d1a4326f
yunusemreozdemir <emreozdemir@mail.com>
2024-04-08 11:21:34 +0300

    init: create get-saved-queries route

---
+++ app/api/get-saved-queries/route.ts
@@ -0,0 +1,16 @@
+import { Redis } from '@upstash/redis';
+
+const redis = new Redis({
+    url: process.env.UPSTASH_REDIS_REST_URL as string,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN as string,
+});
+
+export async function GET() {
+    const queries = await redis.lrange("saved:queries", 0, 49); 
+    
+    if (!queries) {
+        return Response.json([]);
+    }
+
+    return Response.json(queries);
+}
