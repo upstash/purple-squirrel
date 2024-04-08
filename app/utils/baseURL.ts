d09fc13739ef25a17c45435b110304f3c79b801c
yunusemreozdemir <emreozdemir@mail.com>
2024-04-09 02:26:00 +0300

    fix: change base production URL

--- app/utils/baseURL.ts
+++ app/utils/baseURL.ts
@@ -1,6 +1,6 @@
 const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    ? `https://${process.env.NEXT_PUBLIC_URL}`
     : "http://localhost:3000";
 
 export default BASE_URL;
