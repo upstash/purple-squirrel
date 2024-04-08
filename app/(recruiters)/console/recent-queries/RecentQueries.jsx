1b328122df705a82555dc6aa65e0e913c78c9060
yunusemreozdemir <emreozdemir@mail.com>
2024-04-08 11:22:28 +0300

    feat: feed real data to RecentQueries

--- app/(recruiters)/console/recent-queries/RecentQueries.jsx
+++ app/(recruiters)/console/recent-queries/RecentQueries.jsx
@@ -3,41 +3,73 @@
 import React from "react";
 import {  Table,  TableHeader,  TableBody,  TableColumn,  TableRow,  TableCell} from "@nextui-org/table";
 import {Tooltip} from "@nextui-org/tooltip";
-import {columns, queries} from "./mockData.js";
 import ReplayOutlinedIcon from '@mui/icons-material/ReplayOutlined';
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import {Spinner} from "@nextui-org/spinner";
+import {Button} from "@nextui-org/button";
 
+const columns = [
+    {name: "QUERY", uid: "query"},
+    {name: "ACTIONS", uid: "actions"},
+  ];
 
-export default function RecentQueries() {
-  const renderCell = React.useCallback((query, columnKey) => {
-    const cellValue = query[columnKey];
+export default function RecentQueries({
+    recentQueries,
+    setQueryText,
+    loadingRecentQueries,
+    setActiveTab,
+    savedQueries,
+    setSavedQueries
+}) {
+  const renderCell = React.useCallback((item, columnKey) => {
+    const cellValue = item[columnKey];
 
     switch (columnKey) {
       case "query":
         return (
           <div className="flex flex-col pl-unit-2">
-            <p className="text-bold text-sm capitalize">{cellValue}</p>
+            <p className="text-bold text-sm capitalize">{item.query}</p>
           </div>
         );
       case "actions":
         return (
           <div className="relative flex items-center gap-2 justify-end pr-unit-2">
             <Tooltip content="Save Query" color={"primary"} delay={400} closeDelay={600}>
-              <span className="text-lg text-primary-400 cursor-pointer active:opacity-50">
-                <BookmarkBorderIcon />
-              </span>
+              <Button isIconOnly size="sm" variant="light"
+                onPress = {async () => {
+                    setSavedQueries([{id: item.id, query: item.query}, ...savedQueries]);
+                    await fetch("/api/save-query", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify({id: item.id, query: item.query})
+                    });
+                }}
+              >
+                <span className="text-lg text-primary-400 cursor-pointer active:opacity-50">
+                    <BookmarkBorderIcon />
+                </span>
+              </Button>
             </Tooltip>
             <Tooltip content="Reload Query" color={"secondary"} delay={400} closeDelay={600}>
-              <span className="text-lg text-secondary-400 cursor-pointer active:opacity-50">
-                <ReplayOutlinedIcon />
-              </span>
+              <Button isIconOnly size="sm" variant="light"
+                onPress = {async () => {
+                    setQueryText(item.query);
+                    setActiveTab("query-terminal");
+                }}
+              >
+                <span className="text-lg text-secondary-400 cursor-pointer active:opacity-50">
+                    <ReplayOutlinedIcon />
+                </span>
+              </Button>
             </Tooltip>
           </div>
         );
       default:
         return cellValue;
     }
-  }, []);
+  }, [savedQueries, setSavedQueries, setQueryText, setActiveTab]);
 
   return (
     <div className="flex flex-col h-full">
@@ -50,7 +82,7 @@ export default function RecentQueries() {
               </TableColumn>
             )}
           </TableHeader>
-          <TableBody items={queries}>
+          <TableBody items={recentQueries} isLoading={loadingRecentQueries} loadingContent={<Spinner className="h-full w-full bg-default-50/75" label="warning" color="warning" labelColor="warning" />}>
             {(item) => (
               <TableRow key={item.id}>
                 {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
