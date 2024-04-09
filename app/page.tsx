fd351410108a41e3bdacadfdc87348f8c5c7391f
yunusemreozdemir <emreozdemir@mail.com>
2024-04-09 23:55:37 +0300

    feat: fill index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
+import { Divider } from '@nextui-org/react'
+import {SquirrelIcon} from '@primer/octicons-react'
+import {Button} from "@nextui-org/button";
+import {Link} from "@nextui-org/link";
+
 export default function Page() {
-    return <div></div>;
-  }
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <div className="flex flex-col gap-3 p-6 bg-default-50 rounded-xl">
+          <div className="flex items-center justify-start gap-2">
+            <div className="text-secondary">
+              <SquirrelIcon size={72} className="scale-x-[-1]" />
+            </div>
+            <h1 className="text-5xl">Purple Squirrel</h1>
+          </div>
+          <Divider />
+          <Button size="lg" color="secondary" className="mt-2" as={Link} href="/console">Proceed to Console</Button>
+        </div>
+      </div>
+  )};
