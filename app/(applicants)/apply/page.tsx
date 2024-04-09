69492c4bc138ef057cb2454b8b1bc47ac1441d9c
yunusemreozdemir <emreozdemir@mail.com>
2024-04-09 13:06:10 +0300

    refactor: remove unnecessary content

--- app/(applicants)/apply/page.tsx
+++ app/(applicants)/apply/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-    return <h1>Applicant Apply Page</h1>
+    return <div></div>;
   }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-    return <h1>Root Page</h1>
+    return <div></div>;
   }
