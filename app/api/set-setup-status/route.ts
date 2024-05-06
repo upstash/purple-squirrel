25509664d2dc21d0d9e03351da261ae9d25b83b8
yunusemreozdemir <emreozdemir@mail.com>
2024-05-06 12:45:30 +0300

    init: create set-setup-status route

---
+++ app/api/set-setup-status/route.ts
@@ -0,0 +1,17 @@
+import { Redis } from '@upstash/redis';
+import type { NextRequest } from 'next/server';
+
+const redis = new Redis({
+    url: process.env.UPSTASH_REDIS_REST_URL as string,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN as string,
+});
+
+export async function POST(req: NextRequest) {
+    const data = await req.json();
+
+    await redis.set("setup:status", data);
+
+    return Response.json({ status: 200, message: "Success" });
+}
+
+export const dynamic = "force-dynamic";
