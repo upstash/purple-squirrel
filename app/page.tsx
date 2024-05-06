8e6aa766fcd36a89672ccad10f2f95363f34b7bf
yunusemreozdemir <emreozdemir@mail.com>
2024-05-06 12:28:47 +0300

    feat: add setup view to root

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,68 @@
+'use client'
+
+import React, {useState, useEffect} from 'react';
 import { Divider } from '@nextui-org/react'
 import {SquirrelIcon} from '@primer/octicons-react'
 import {Button} from "@nextui-org/button";
 import {Link} from "@nextui-org/link";
+import TerminalOutlinedIcon from '@mui/icons-material/TerminalOutlined';
+import AdminPanelSettingsOutlinedIcon from '@mui/icons-material/AdminPanelSettingsOutlined';
+import InputOutlinedIcon from '@mui/icons-material/InputOutlined';
+
+
 
 export default function Page() {
+    const [setupStatus, setSetupStatus] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch("/api/get-setup-status")
+            .then(res => res.json())
+            .then(data => {
+                setSetupStatus(data);
+                console.log(data);
+                setLoading(false);
+            });
+    }, []);
+
     return (
-      <div className="h-screen flex items-center justify-center">
-        <div className="flex flex-col gap-3 p-6 bg-default-50 rounded-medium">
-          <div className="flex items-center justify-start gap-2">
-            <div className="text-secondary">
-              <SquirrelIcon size={72} className="scale-x-[-1]" />
+      <div className="h-screen flex items-center justify-center bg-gradient-to-br from-blue-800 to-purple-500">
+        {
+          loading
+          ?
+            null
+          :
+            <div className="flex flex-col gap-3 p-6 bg-default-50 rounded-medium">
+              {
+                !setupStatus
+                ?
+                  <div className="flex items-center justify-center gap-2">
+                    <h1 className="text-3xl text-center text-default-900">Welcome to</h1>
+                    <div className="text-secondary">
+                        <SquirrelIcon size={48} className="scale-x-[-1]" />
+                    </div>
+                    <h1 className="text-3xl">Purple Squirrel!</h1>
+                  </div>
+                :
+                  <div className="flex items-center justify-center gap-2">
+                    <div className="text-secondary">
+                      <SquirrelIcon size={72} className="scale-x-[-1]" />
+                    </div>
+                    <h1 className="text-5xl">Purple Squirrel</h1>
+                  </div>
+              }
+              <Divider />
+              {
+                !setupStatus
+                ?
+                  <Button size="lg" variant="solid" color="secondary" className="w-full text-large mt-2" as={Link} href="/setup" startContent={<InputOutlinedIcon />}>Application Setup</Button>
+                :
+                  <div className="flex flex-col mt-2 gap-2">
+                    <Button size="lg" variant="solid" color="secondary" className="w-full text-large" as={Link} href="/console" startContent={<TerminalOutlinedIcon />}>Recruiter Console</Button>
+                    <Button size="lg" variant="bordered" color="secondary" className="w-full text-large" as={Link} href="/dashboard" startContent={<AdminPanelSettingsOutlinedIcon />}>Admin Dashboard</Button>
+                  </div>
+              }
             </div>
-            <h1 className="text-5xl">Purple Squirrel</h1>
-          </div>
-          <Divider />
-          <Button size="lg" color="secondary" className="mt-2" as={Link} href="/console">Proceed to Console</Button>
-        </div>
+        }
       </div>
   )};
