8e01c30a780c6de526025991c22ba4ae299f3010
yunusemreozdemir <emreozdemir@mail.com>
2024-05-21 11:35:32 +0300

    feat: RBAC related global types

---
+++ types/globals.d.ts
@@ -0,0 +1,11 @@
+export { };
+
+export type Roles = "admin" | "recruiter" | "applicant";
+
+declare global {
+  interface CustomJwtSessionClaims {
+    metadata: {
+      role?: Roles
+    };
+  }
+}
