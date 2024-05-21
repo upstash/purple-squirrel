fbcb6922ba5ecd90a1174480a68bc9f4967a6f3c
yunusemreozdemir <emreozdemir@mail.com>
2024-05-21 11:35:45 +0300

    feat: create setRole action

---
+++ app/(administrators)/dashboard/_actions.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import { checkRole } from "@/app/utils/roles";
+import { Roles } from "@/types/globals";
+import { clerkClient } from "@clerk/nextjs/server";
+
+export async function setRole(id: string, role: Roles) {
+  if (!checkRole("admin")) {
+    return { message: "Not Authorized" };
+  }
+
+  try {
+    const res = await clerkClient.users.updateUser(
+      id,
+      {
+        publicMetadata: { role: role },
+      }
+    );
+    return { message: res.publicMetadata };
+  } catch (err) {
+    return { message: err };
+  }
+}
