d5265291057c8dbfc13c8524f591980a153454bd
yunusemreozdemir <emreozdemir@mail.com>
2024-05-23 22:24:38 +0300

    feat: add maxFileCount limit for file uploads

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -8,7 +8,7 @@ const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  pdfUploader: f({ pdf: { maxFileSize: "1MB" } })
+  pdfUploader: f({ pdf: { maxFileSize: "1MB", maxFileCount: 1 } })
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
       // This code runs on your server before upload
