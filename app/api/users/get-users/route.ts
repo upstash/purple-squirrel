5903eae3f1db4b536dd78f59065918eb93935714
yunusemreozdemir <emreozdemir@mail.com>
2024-05-23 22:24:55 +0300

    feat: create get-users route

---
+++ app/api/users/get-users/route.ts
@@ -0,0 +1,15 @@
+import { clerkClient } from "@clerk/nextjs/server";
+import type { NextRequest } from 'next/server';
+
+export async function POST(req: NextRequest) {
+    const data = await req.json();
+    if (!(data?.query)) {
+        return Response.error();
+    }
+    const query = data.query;
+    const res = await clerkClient.users.getUserList({ query });
+    const users = res.data;
+    return Response.json({ status: 200, users: users });
+}
+
+export const dynamic = "force-dynamic";
