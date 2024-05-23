f124d72f8e7f82cdc2b3f328c5befee030d56e5d
yunusemreozdemir <emreozdemir@mail.com>
2024-05-23 22:20:45 +0300

    feat: update home page to make profile completion checks

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useClerk } from '@clerk/nextjs';
+import { useUser } from "@clerk/clerk-react";
 import React, {useState, useEffect} from 'react';
 import { Divider } from '@nextui-org/react'
 import {SquirrelIcon} from '@primer/octicons-react'
@@ -9,24 +9,38 @@ import {Link} from "@nextui-org/link";
 import TerminalOutlinedIcon from '@mui/icons-material/TerminalOutlined';
 import AdminPanelSettingsOutlinedIcon from '@mui/icons-material/AdminPanelSettingsOutlined';
 import InputOutlinedIcon from '@mui/icons-material/InputOutlined';
-
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import WorkOutlineOutlinedIcon from '@mui/icons-material/WorkOutlineOutlined';
+import FormatListBulletedOutlinedIcon from '@mui/icons-material/FormatListBulletedOutlined';
+import { useRouter } from 'next/navigation'
 
 
 export default function Page() {
-    const { signOut } = useClerk();
-
+    const router = useRouter()
+    const { isSignedIn, user, isLoaded } = useUser();
     const [setupStatus, setSetupStatus] = useState(false);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        fetch("/api/get-setup-status")
+        fetch("/api/settings/get-settings")
             .then(res => res.json())
             .then(data => {
-                setSetupStatus(data);
-                console.log(data);
+                setSetupStatus(data.setupStatus);
                 setLoading(false);
             });
     }, []);
+
+    if (!isLoaded || !user) {
+      return null;
+    }
+
+    const authRole = user.publicMetadata?.role;
+    const profileComplete = user.publicMetadata?.complete;
+
+    if (authRole === "recruiter") {
+      router.push('/console')
+      return null;
+    }
 
     return (
       <div className="h-screen flex items-center justify-center bg-gradient-to-br from-blue-800 to-purple-500">
@@ -36,34 +50,47 @@ export default function Page() {
             null
           :
             <div className="flex flex-col gap-3 p-6 bg-default-50 rounded-medium">
-              {
-                !setupStatus
-                ?
-                  <div className="flex items-center justify-center gap-2">
-                    <h1 className="text-3xl text-center text-default-900">Welcome to</h1>
-                    <div className="text-secondary">
-                        <SquirrelIcon size={48} className="scale-x-[-1]" />
-                    </div>
-                    <h1 className="text-3xl">Purple Squirrel!</h1>
-                  </div>
-                :
-                  <div className="flex items-center justify-center gap-2">
-                    <div className="text-secondary">
-                      <SquirrelIcon size={72} className="scale-x-[-1]" />
-                    </div>
-                    <h1 className="text-5xl">Purple Squirrel</h1>
-                  </div>
-              }
+              <div className="flex items-center justify-center gap-2">
+                <div className="text-secondary">
+                  <SquirrelIcon size={72} className="scale-x-[-1]" />
+                </div>
+                <h1 className="text-5xl">Purple Squirrel</h1>
+              </div>
               <Divider />
               {
-                !setupStatus
-                ?
-                  <Button size="lg" variant="solid" color="secondary" className="w-full text-large mt-2" as={Link} href="/setup" startContent={<InputOutlinedIcon />}>Application Setup</Button>
-                :
-                  <div className="flex flex-col mt-2 gap-2">
-                    <Button size="lg" variant="solid" color="secondary" className="w-full text-large" as={Link} href="/console" startContent={<TerminalOutlinedIcon />}>Recruiter Console</Button>
-                    <Button size="lg" variant="bordered" color="secondary" className="w-full text-large" as={Link} href="/dashboard" startContent={<AdminPanelSettingsOutlinedIcon />}>Admin Dashboard</Button>
-                  </div>
+                (
+                  !authRole &&
+                  <span>You are not authorized yet, contact your administrator.</span>
+                )
+              }
+              {
+                (
+                  authRole === "admin" &&
+                  (
+                    !setupStatus
+                    ?
+                      <Button size="lg" variant="solid" color="secondary" className="w-full text-large mt-2" as={Link} href="/setup" startContent={<InputOutlinedIcon />}>Application Setup</Button>
+                    :
+                      <div className="flex flex-col mt-2 gap-2">
+                        <Button size="lg" variant="solid" color="secondary" className="w-full text-large" as={Link} href="/console" startContent={<TerminalOutlinedIcon />}>Recruiter Console</Button>
+                        <Button size="lg" variant="bordered" color="secondary" className="w-full text-large" as={Link} href="/dashboard" startContent={<AdminPanelSettingsOutlinedIcon />}>Admin Dashboard</Button>
+                      </div>
+                  )
+                )
+              }
+              {
+                (
+                  authRole === "applicant" &&
+                  (
+                    !profileComplete
+                    ?
+                      <Button size="lg" variant="solid" color="warning" className="w-full text-large mt-2" as={Link} href="/profile" startContent={<AccountCircleOutlinedIcon />}>Complete Your Profile</Button>
+                    :
+                      <div className="flex flex-col mt-2 gap-2">
+                        <Button size="lg" variant="solid" color="secondary" className="w-full text-large" as={Link} href="/applications" startContent={<WorkOutlineOutlinedIcon />}>Applications</Button>
+                      </div>
+                  )
+                )
               }
             </div>
         }
