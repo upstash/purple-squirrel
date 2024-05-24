25f93954d9133637440adbd4b5efb76064f3bce4
yunusemreozdemir <emreozdemir@mail.com>
2024-05-24 15:26:35 +0300

    feat: make style changes in sign-in and sign-up pages

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="h-screen flex items-center justify-center bg-gradient-to-br from-blue-800 to-purple-500">
+      <SignIn />;
+    </div>
+  )
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="h-screen flex items-center justify-center bg-gradient-to-br from-blue-800 to-purple-500">
+      <SignUp />;
+    </div>
+  )
 }
