d9c409aa5c22dbf11487b7c7885355f8e925a575
yunusemreozdemir <emreozdemir@mail.com>
2024-06-11 11:47:42 +0300

    feat: migrate route, add recent query history separation by user

--- app/api/query/push-recent-query/route.ts
+++ app/api/query/push-recent-query/route.ts
@@ -1,5 +1,6 @@
 import { Redis } from '@upstash/redis';
 import type { NextRequest } from 'next/server';
+import { auth } from "@clerk/nextjs/server"
 
 const redis = new Redis({
     url: process.env.UPSTASH_REDIS_REST_URL as string,
@@ -7,10 +8,11 @@ const redis = new Redis({
 });
 
 export async function POST(req: NextRequest) {
+    const { userId } = auth()
     const data = await req.json();
 
-    await redis.lpush("recent:queries", data); 
-    await redis.ltrim("recent:queries", 0, 29); 
+    await redis.lpush(`recent:queries#${userId}`, data); 
+    await redis.ltrim(`recent:queries#${userId}`, 0, 29); 
 
     return Response.json({ status: 200, message: "Success" });
 }
