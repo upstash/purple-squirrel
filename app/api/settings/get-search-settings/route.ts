8bee4eb407b805183ff28c577f20e69cf01f2a70
yunusemreozdemir <emreozdemir@mail.com>
2024-06-11 11:43:51 +0300

    feat: migrate route, add search settings separation by user

--- app/api/settings/get-search-settings/route.ts
+++ app/api/settings/get-search-settings/route.ts
@@ -1,4 +1,5 @@
 import { Redis } from '@upstash/redis';
+import { auth } from "@clerk/nextjs/server"
 
 const redis = new Redis({
     url: process.env.UPSTASH_REDIS_REST_URL as string,
@@ -6,10 +7,12 @@ const redis = new Redis({
 });
 
 export async function GET() {
-    const value = await redis.json.get("search:settings", "$");
+    const { userId } = auth()
+
+    const value = await redis.json.get(`search:settings#${userId}`, "$");
     
     if (!value || !Array.isArray(value) || value.length !== 1) {
-        return Response.json({deepTopK: 10, flashTopK: 20, deepWeight: 0.5, flash: false});
+        return Response.json({deepTopK: 10, flashTopK: 20, deepWeight: 0.5, flash: true});
     }
 
     return Response.json(value[0]);
