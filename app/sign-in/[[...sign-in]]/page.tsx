8841d8b2d7d5a5f138911197067e520c9691c6f7
yunusemreozdemir <emreozdemir@mail.com>
2024-06-11 19:39:15 +0300

    fix: remove sign-in, sign-up extra colon

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="h-screen flex items-center justify-center bg-gradient-to-br from-blue-800 to-purple-500">
-      <SignIn />;
+      <SignIn />
     </div>
   )
 }
