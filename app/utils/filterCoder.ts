ef44ca85d9c77ce9ec4d499831a9aa326a662ca7
yunusemreozdemir <emreozdemir@mail.com>
2024-06-11 11:53:53 +0300

    feat: remove education and position filters

--- app/utils/filterCoder.ts
+++ app/utils/filterCoder.ts
@@ -2,9 +2,6 @@ import { Filter } from '@/types/types';
 
 export default function filterCoder(filter: Filter) {
     const filterQuery = [];
-    if (filter.positionFilter) {
-        filterQuery.push(`position = '${filter.positionFilter}'`);
-    }
     if (filter.countryCodeFilter && filter.countryCodeFilter.length > 0) {
         const countryCodes = filter.countryCodeFilter.map((countryCode) => `'${countryCode}'`).join(", ");
         filterQuery.push(`countryCode IN (${countryCodes})`);
@@ -22,28 +19,6 @@ export default function filterCoder(filter: Filter) {
         }
         if (filter.yoeFilter.max !== null && filter.yoeFilter.max !== -1) {
             filterQuery.push(`yoe <= ${filter.yoeFilter.max}`);
-        }
-    }
-    if (filter.degreeFilter && filter.degreeFilter.length > 0) {
-        const degrees = filter.degreeFilter.map((degree) => `'${degree}'`).join(", ");
-        filterQuery.push(`highestDegree IN (${degrees})`);
-    }
-    if (filter.graduationDateFilter !== null) {
-        if (filter.graduationDateFilter.min !== null && filter.graduationDateFilter.min.year !== -1) {
-            const min = filter.graduationDateFilter.min;
-            if (min.month === -1) {
-                filterQuery.push(`graduationYear >= ${min.year}`);
-            } else {
-                filterQuery.push(`(graduationYear > ${min.year} OR (graduationYear = ${min.year} AND graduationMonth >= ${min.month}))`);
-            }
-        }
-        if (filter.graduationDateFilter.max !== null && filter.graduationDateFilter.max.year !== -1 && filter.graduationDateFilter.max.month !== -1) {
-            const max = filter.graduationDateFilter.max;
-            if (max.month === -1) {
-                filterQuery.push(`graduationYear <= ${max.year}`);
-            } else {
-                filterQuery.push(`(graduationYear < ${max.year} OR (graduationYear = ${max.year} AND graduationMonth <= ${max.month}))`);
-            }
         }
     }
     if (filterQuery.length === 0) {
