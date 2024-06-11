a7aba952cb26d2153f05326bfc505d2225eb8839
yunusemreozdemir <emreozdemir@mail.com>
2024-06-11 12:31:36 +0300

    feat: check application availability (setup status, application methods, position status)

--- app/api/positions/get-position-by-id/route.ts
+++ app/api/positions/get-position-by-id/route.ts
@@ -9,13 +9,35 @@ const redis = new Redis({
 export async function POST(req: NextRequest) {
     const body = await req.json();
     const positionId = body.id;
+
+    const setupStatus = await redis.get("setup:status");
+    if (!setupStatus) {
+        return Response.json({
+            status: 400,
+            message: "setup",
+        });
+    }
+    
+    const methodsValue = await redis.get("application:methods");
+    if (!methodsValue || !Array.isArray(methodsValue) || methodsValue.length === 0 || !methodsValue.includes("ps")) {
+        return Response.json({
+            status: 400,
+            message: "method",
+        });
+    }
+
     const positions = await redis.lrange("positions", 0, -1);
     if (positions.length === 0) {
-        return Response.json({ status: 404, message: "No positions found" });
+        return Response.json({ status: 404, message: "not-found" });
     }
     const position = positions.find((p: any) => p.id === positionId);
     if (!position) {
-        return Response.json({ status: 404, message: "Position not found" });
+        return Response.json({ status: 404, message: "not-found" });
+    }
+    const parsedPosition = JSON.parse(position);
+
+    if (parsedPosition.status !== "open") {
+        return Response.json({ status: 400, message: "closed" });
     }
     return Response.json(position);
 }
