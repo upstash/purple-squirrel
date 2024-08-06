a3294ee5e29eb1b1b6d2f90a637f7a773cea457a
yunusemreozdemir <emreozdemir@mail.com>
2024-08-06 15:17:31 +0200

    feat: create AdminLayout with AdminManager

---
+++ app/(administrators)/layout.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { AdminManager } from "@/app/managers";
+
+export default function AdminLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <section>
+        <AdminManager>
+            {children}
+        </AdminManager>
+      </section>
+    )
+  }
