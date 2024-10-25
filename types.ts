1ffd0eee98bb0f284d2d399982ed9b688f5fc770
yunusemreozdemir <emreozdemir@mail.com>
2024-10-25 15:39:29 +0300

    feat: create applicant type

---
+++ types.ts
@@ -0,0 +1,11 @@
+export type Applicant = {
+  id: string;
+  name: string;
+  location: string;
+  position: string;
+  resumeUrl: string;
+  coverLetter: string;
+  favorite: boolean;
+  archived: boolean;
+  notes: string;
+};
