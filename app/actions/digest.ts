4baeb5ff28f53f4fa2de3d2963ed6aa68200b9d3
yunusemreozdemir <emreozdemir@mail.com>
2024-10-27 19:33:43 +0300

    feat: create digest action

---
+++ app/actions/digest.ts
@@ -0,0 +1,58 @@
+"use server";
+
+import { Client } from "@upstash/qstash";
+import QSTASH_TARGET_URL from "@/lib/qstash-target-url";
+
+const client = new Client({ token: process.env.QSTASH_TOKEN as string });
+
+export async function digest({
+  folder,
+  quantity,
+  interval,
+}: {
+  folder: string;
+  quantity: number;
+  interval: "minutes" | "hours";
+}) {
+  const queue = client.queue({
+    queueName: "mail-fetch-queue",
+  });
+
+  await queue.upsert({
+    parallelism: 2,
+  });
+
+  await client.publishJSON({
+    url: `${QSTASH_TARGET_URL}/api/search-unseen`,
+    method: "POST",
+    retries: 0,
+    body: JSON.stringify({
+      folder,
+    }),
+  });
+  if (process.env.NODE_ENV === "production") {
+    const cron =
+      interval === "minutes"
+        ? `*/${quantity} * * * *`
+        : `0 */${quantity} * * *`;
+
+    const schedules = client.schedules;
+    const allSchedules = await schedules.list();
+    for (let i = 0; i < allSchedules.length; i++) {
+      if (
+        allSchedules[i].destination === `${QSTASH_TARGET_URL}/api/search-unseen`
+      ) {
+        await schedules.delete(allSchedules[i].scheduleId);
+      }
+    }
+
+    await schedules.create({
+      destination: `${QSTASH_TARGET_URL}/api/search-unseen`,
+      cron,
+      retries: 0,
+      body: JSON.stringify({
+        folder,
+      }),
+    });
+  }
+}
