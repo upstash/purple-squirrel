e8821564c2605f01d1a0a7702f29bf32702823fc
yunusemreozdemir <emreozdemir@mail.com>
2024-10-27 19:33:31 +0300

    feat: simplify home page with components

--- app/page.tsx
+++ app/page.tsx
@@ -1,127 +1,56 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { Card } from "@/components/ui/card";
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import {
-  Pencil2Icon,
-  StarIcon,
-  ArchiveIcon,
-  MagnifyingGlassIcon,
-} from "@radix-ui/react-icons";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+"use client";
 
-import { mockData } from "./mockData";
+import { useState } from "react";
+import { Spinner } from "@/components/ui/spinner";
+
+import QueryBar from "@/components/query-bar";
+import ApplicantTable from "@/components/table";
+import ExampleQueries from "@/components/example-queries";
+
+import type { Applicant } from "@/types";
+import { search } from "@/app/actions/search";
+import { update } from "@/app/actions/update";
 
 export default function Home() {
+  const [applicants, setApplicants] = useState<Applicant[]>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [query, setQuery] = useState<string>("");
+
+  async function onSearch(query: string) {
+    setLoading(true);
+    const response = await search(query);
+    setApplicants(response);
+    setLoading(false);
+  }
+
+  async function onUpdate(applicant: Applicant) {
+    setApplicants((applicants) =>
+      applicants?.map((a) => (a.id === applicant.id ? applicant : a))
+    );
+    await update(applicant);
+  }
+
   return (
     <div className="flex flex-col items-center justify-between min-h-screen p-10">
-      <main className="flex flex-col gap-6 row-start-2 items-center sm:items-start min-w-[804px] pt-16">
+      <main className="flex flex-col gap-6 row-start-2 items-center sm:items-start w-[860px] pt-16">
         <div className="text-violet-600 font-bold text-3xl">
           Purple Squirrel
         </div>
-        <div className="flex flex-col gap-3">
-          <div className="flex flex-row gap-2 w-full">
-            <Input placeholder="Search applicants..." className="w-full" />
-            <Button>
-              <MagnifyingGlassIcon /> Search
-            </Button>
-          </div>
-          <div className="flex flex-row w-full justify-center">
-            <Tabs defaultValue="active">
-              <TabsList>
-                <TabsTrigger value="active">Active</TabsTrigger>
-                <TabsTrigger value="favorites">Favorites</TabsTrigger>
-                <TabsTrigger value="archived">Archived</TabsTrigger>
-              </TabsList>
-            </Tabs>
-          </div>
-          <Card className="p-1">
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead className="w-[100px]">Name</TableHead>
-                  <TableHead>Location</TableHead>
-                  <TableHead>Position</TableHead>
-                  <TableHead className="pl-6">Resume</TableHead>
-                  <TableHead className="pl-6">Cover Letter</TableHead>
-                  <TableHead className="pl-5">Actions</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {mockData.map((applicant) => (
-                  <TableRow key={applicant.id}>
-                    <TableCell>{applicant.name}</TableCell>
-                    <TableCell>{applicant.location}</TableCell>
-                    <TableCell>{applicant.position}</TableCell>
-                    <TableCell>
-                      <Button variant="link">
-                        <a
-                          href={applicant.resumeUrl}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          View Resume
-                        </a>
-                      </Button>
-                    </TableCell>
-                    <TableCell>
-                      <Dialog>
-                        <DialogTrigger>
-                          <Button variant="link">View Cover Letter</Button>
-                        </DialogTrigger>
-                        <DialogContent>
-                          <DialogHeader>
-                            <DialogTitle>Cover Letter</DialogTitle>
-                            <DialogDescription>
-                              {applicant.coverLetter}
-                            </DialogDescription>
-                          </DialogHeader>
-                        </DialogContent>
-                      </Dialog>
-                    </TableCell>
-                    <TableCell className="text-right">
-                      <Button
-                        variant="ghost"
-                        size="icon"
-                        className="hover:text-violet-700"
-                      >
-                        <Pencil2Icon />
-                      </Button>
-                      <Button
-                        variant="ghost"
-                        size="icon"
-                        className="hover:text-violet-700"
-                      >
-                        <StarIcon />
-                      </Button>
-                      <Button
-                        variant="ghost"
-                        size="icon"
-                        className="hover:text-violet-700"
-                      >
-                        <ArchiveIcon />
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </Card>
+        <div className="flex flex-col gap-3 w-full">
+          <QueryBar query={query} setQuery={setQuery} onSearch={onSearch} />
+          {loading ? (
+            <Spinner className="text-violet-700" />
+          ) : applicants ? (
+            applicants.length > 0 ? (
+              <ApplicantTable applicants={applicants} onUpdate={onUpdate} />
+            ) : (
+              <div className="text-zinc-500 text-center">
+                No applicants found.
+              </div>
+            )
+          ) : (
+            <ExampleQueries setQuery={setQuery} onSearch={onSearch} />
+          )}
         </div>
       </main>
       <footer className="flex gap-6 flex-wrap items-center justify-center">
