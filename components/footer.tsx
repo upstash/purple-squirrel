417166c6353218ef510ab6fec73ec37c65dc74e0
Adem ilter <ademilter@gmail.com>
2024-11-04 22:40:36 +0300

    style

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-center py-10">
+    <footer className="flex items-center justify-center py-10 text-sm">
       <a href="https://upstash.com/" target="_blank" rel="noopener noreferrer">
         Created with Upstash →
       </a>
