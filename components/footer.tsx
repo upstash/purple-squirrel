02c296c815ee638738a5211a485736ee3217fbee
Adem ilter <ademilter@gmail.com>
2024-11-04 23:46:55 +0300

    fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,21 @@
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-center py-10">
-      <a href="https://upstash.com/" target="_blank" rel="noopener noreferrer">
-        Created with Upstash →
+    <footer className="fixed inset-1 top-auto rounded-md bg-zinc-900 px-8 py-4 text-center text-sm text-white">
+      Open source project on{" "}
+      <a
+        href="https://github.com/upstash/purple-squirrel"
+        target="_blank"
+        className="underline"
+      >
+        GitHub
+      </a>{" "}
+      • Powered by{" "}
+      <a
+        href="https://upstash.com/?utm_source=purple-squirrel"
+        target="_blank"
+        className="underline"
+      >
+        Upstash
       </a>
     </footer>
   );
